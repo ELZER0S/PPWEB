a217fb2f041615803fca2500f603f8e166e30aac
Backer <AsanRodnuan2546@gmail.com>
2024-11-11 14:33:06 +0700

    Refactor navigation links and implement user session management in index.php and logout.php

--- src/html/index.php
+++ src/html/index.php
@@ -1,3 +1,32 @@
+<?php
+
+    session_start();
+    include("../../include/include_db_oo.php");
+
+    $quiz = "SELECT * FROM question";
+    $result = $conn->query($quiz);
+
+
+    //check ว่ามีกี่ row
+    $row = $result->num_rows;
+
+
+    if(!isset($_SESSION['users_id'])) {
+        header('Location: ../../login.php');
+    }
+    
+    $users_id = $_SESSION['users_id'];
+
+    $get_data_name = "SELECT * FROM users WHERE users_id = $users_id";
+    $get_data_name_result = $conn->query($get_data_name);
+
+    $get_data_name_row = $get_data_name_result->fetch_assoc();
+
+
+
+?>
+
+
 <!doctype html>
 <html lang="en">
 
@@ -26,9 +55,97 @@
         <!--  Row 1 -->
         <div class="row">
           <div class="col-lg text-white">
-            <h1 class="text-white">ยินดีตอนรับ</h1>
+            <h1 class="text-white">ยินดีต้อนรับ</h1>
+            <p>ชื่อผู้ใช้: <?php echo $get_data_name_row["users_name"] ?></p>
           </div>
         </div>
+        <!--  Row 2 -->
+        <div class="row">
+          <div class="col-md-6">
+            <div class="card bg-dark text-white">
+              <div class="card-body">
+                <h5 class="card-title text-white">จำนวนแบบทดสอบทั้งหมด</h5>
+                <p class="card-text">20 ข้อ</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card bg-dark text-white">
+              <div class="card-body">
+                <h5 class="card-title text-white">จำนวนผู้เข้าร่วม</h5>
+                <p class="card-text"><?php echo $row ?> ครั้ง</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  Row 3 -->
+        <div class="row mt-5">
+        <div class="col-md-4">
+          <div class="card bg-info text-white">
+            <div class="card-body">
+              <h5 class="card-title">จำนวนแบบสอบถามทั้งหมด</h5>
+              <p class="card-text">20 ข้อ</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card bg-success text-white">
+            <div class="card-body">
+              <h5 class="card-title">จำนวนผู้ตอบทั้งหมด</h5>
+              <p class="card-text"><?php echo $row ?> ครั้ง</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card bg-warning text-white">
+            <div class="card-body">
+              <h5 class="card-title">คำตอบเฉลี่ยต่อแบบสอบถาม</h5>
+              <p class="card-text">20 คำตอบ</p>
+            </div>
+          </div>
+        </div>
+        </div>
+        <!--  Row 4 -->
+        <div class="row mt-4">
+                <div class="col-md-12">
+                  <h3 class="text-white">รายการแบบสอบถาม</h3>
+                  <table class="table table-dark table-striped">
+                    <thead>
+                      <tr>
+                        <th>ชื่อผู้ตอบแบบสอบถาม</th>
+                        <th>จำนวนคำตอบ</th>
+                        <th>วันที่สร้าง</th>
+                        <th>ตัวเลือก</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+        <?php
+
+            $data_quiz = "SELECT * FROM question";
+            $result = $conn->query($data_quiz);
+
+            //ดึงข้อมูลทั้งหมดลงมา
+            while($row = $result->fetch_assoc()) {
+                $get_name = $row['question_users_id'];
+
+                $get_name_sql = "SELECT * FROM users WHERE users_id = $get_name";
+                $get_name_result = $conn->query($get_name_sql);
+                $get_name_row = $get_name_result->fetch_assoc();
+
+                ?>
+                  <tr>
+                    <td><?php echo $get_name_row['users_name']?></td>
+                    <td>20 คำตอบ</td>
+                    <td>2023-11-10</td>
+                    <td>
+                      <a href="quiz.php" class="btn btn-primary">ดูข้อมูล</a>
+                    </td>
+                    </tr>
+                  </tbody>
+            <?php } ?>
+            </table>
+            </div>
+            </div>
       </div>
     </div>
   </div>
